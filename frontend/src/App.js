efa9d1a3ddb39ed2650c766dc5cd8d827abade9d
Giovanna Silva <giovannapergentinocs@gmail.com>
2024-05-31 10:03:07 -0300

    settings workflows

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ function App() {
   return (
     <>
       <Container>
-        <Title>USUÁRIOS</Title>
+        <Title>USUÁRIOS - Giovanna Pergentino</Title>
         <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
         <Grid setOnEdit={setOnEdit} users={users} setUsers={setUsers} />
       </Container>
