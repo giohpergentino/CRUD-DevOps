6160018d6a2bf79a613940f93f996d8a84927dcf
Giovanna Silva <giovannapergentinocs@gmail.com>
2024-06-01 22:32:46 -0300

    testing ec2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,7 +41,7 @@ function App() {
   return (
     <>
       <Container>
-        <Title>CADASTRO DE USUÁRIOS</Title>
+        <Title>FAÇA O SEU CADASTRO</Title>
         <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
         <Grid setOnEdit={setOnEdit} users={users} setUsers={setUsers} />
       </Container>
