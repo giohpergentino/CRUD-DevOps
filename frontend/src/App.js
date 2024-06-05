172081cf74035e7e2e124d130f353e407ed1873a
Giovanna Silva <giovannapergentinocs@gmail.com>
2024-06-05 18:34:07 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,7 +41,7 @@ function App() {
   return (
     <>
       <Container>
-        <Title>CADASTRO DE USUÁRIOS - TESTE</Title>
+        <Title>CADASTRO DE USUÁRIOS</Title>
         <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
         <Grid setOnEdit={setOnEdit} users={users} setUsers={setUsers} />
       </Container>
