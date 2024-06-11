315f1340d2ecd2e1d39a8f5429a8475ef9713d6a
Giovanna Silva <giovannapergentinocs@gmail.com>
2024-06-11 15:29:27 -0300

    new settings

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -117,7 +117,7 @@ const Form = ({ getUsers, onEdit, setOnEdit }) => {
         <Input name="data_nascimento" type="date" />
       </InputArea>
 
-      <Button type="submit">SALVAR TESTEEEEEE 3</Button>
+      <Button type="submit">SALVAR TESTE</Button>
     </FormContainer>
   );
 };
